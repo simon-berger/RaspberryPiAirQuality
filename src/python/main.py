60cade80fe633bf9b06ebaea07a333b588f8d9d7
Simon Berger <simon@hairtex.de>
2022-01-08 08:30:29 +0000

    Implemented simple air quality program: - Uses only bme688 - Rrints several interesting values to the console - Runs forever until error or keyboard interrupt - Values are rounded - Values are updated every second

--- src/python/main.py
+++ src/python/main.py
@@ -1,4 +1,35 @@
 from sensor.bme688 import BME688
+from time import sleep
+import decimal
 
+# Create sensor object
 bme = BME688()
-print(bme.read())
+
+try:
+    while True:
+        # Read the data
+        d = bme.read()
+
+        # Round all data values
+        for d_key in d:
+            d[d_key] = float(round(d[d_key], 2))
+
+        # Print several values
+        split_char = "-"
+        print("Sample Nr:", d["sample_nr"], split_char,\
+            "IAQ:", d["iaq"], split_char,\
+            "IAQ Acc:", d["iaq_accuracy"], split_char,\
+            "Temp:", d["temperature"], split_char,\
+            "Hum:", d["humidity"], split_char,\
+            "C02:", d["co2_equivalent"], split_char,\
+            "C02 Acc:", d["co2_accuracy"], split_char,\
+            "Comp Gas:", d["comp_gas_value"], split_char,\
+            "Comp Gas Acc:", d["comp_gas_accuracy"], split_char,\
+            end="\r")
+
+        # Update every second
+        sleep(1)
+except KeyboardInterrupt:
+    # Interrupt received
+    print("Program stopped")
+    exit(0)
